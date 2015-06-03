bc61419880da2cb34710f3fbc9443cd1c39d371b
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-03 11:28:49 -0500

    Fix - use deployment configuration values

--- rdsmysqlbackup/recipes/backup.rb
+++ rdsmysqlbackup/recipes/backup.rb
@@ -2,15 +2,16 @@
 # - Backups 
 
 # Backup existing database 
-
-Chef::Log.info("Backup current Wordpress database")
-script "mysql_backup" do
-	interpreter "bash"
-	user "root"
-	cwd "${node[:default][:rdsbackup][:storage]}/"
-	code <<-EOH
-		/usr/bin/mysqldump -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} \
-			| gzip \
-			> #{deploy[:database][:database]}_$(date +%Y-%m-%d-%H-%M-%S).dmp.gz
-	EOH
+node[:deploy].each do |app_name, deploy|
+	Chef::Log.info("Backup current Wordpress database")
+	script "mysql_backup" do
+		interpreter "bash"
+		user "root"
+		cwd "${default[:rdsbackup][:storage]}/"
+		code <<-EOH
+			/usr/bin/mysqldump -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} \
+				| gzip \
+				> #{deploy[:database][:database]}_$(date +%Y-%m-%d-%H-%M-%S).dmp.gz
+		EOH
+	end
 end
