824417098f99f8996127548a4ec4e08ece16f76b
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-04 19:38:20 -0500

    Test command for automated rds snapshot

--- rdssnapshot/recipes/snapshot.rb
+++ rdssnapshot/recipes/snapshot.rb
@@ -1,4 +1,17 @@
 # AWS OpsWorks Recipe for RDS that creates snapshots.
 # - Creates a snapshot of associated RDS instances
 
-Chef::Log.info("#{node['clipmx']['rds']['instancename']}")
+Chef::Log.info("Creating snapshot for: #{node['clipmx']['rds']['instancename']}")
+
+Chef::Log.info("Importing Wordpress database backup...")
+script "create_snapshot" do
+	interpreter "bash"
+	
+	
+	code <<-EOH
+		aws rds aws rds create-db-snapshot \
+			--region #{node['clipmx']['rds']['region']} \
+			--db-snapshot-identifier=#{node['clipmx']['rds']['instancename']}-$(date +%Y-%m-%d-%H-%M-%S) \
+			--db-instance-identifier #{node['clipmx']['rds']['instancename']}
+	EOH
+end
