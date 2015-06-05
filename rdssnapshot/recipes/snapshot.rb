31b66a728231c5f2859f70786ef0d88897e61a1f
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-04 19:51:52 -0500

    Test command for automated rds snapshot

--- rdssnapshot/recipes/snapshot.rb
+++ rdssnapshot/recipes/snapshot.rb
@@ -5,9 +5,9 @@ Chef::Log.info("Creating snapshot for: #{node['clipmx']['rds']['instancename']}"
 
 Chef::Log.info("Importing Wordpress database backup...")
 Chef::Log.info("aws rds aws rds create-db-snapshot \
-			--region #{node['clipmx']['rds']['region']} \
+			--region=#{node['clipmx']['rds']['region']} \
 			--db-snapshot-identifier=#{node['clipmx']['rds']['instancename']}-$(date +%Y-%m-%d-%H-%M-%S) \
-			--db-instance-identifier #{node['clipmx']['rds']['instancename']}")
+			--db-instance-identifier=#{node['clipmx']['rds']['instancename']}")
 			
 script "create_snapshot" do
 	interpreter "bash"
@@ -15,8 +15,8 @@ script "create_snapshot" do
 	
 	code <<-EOH
 		aws rds aws rds create-db-snapshot \
-			--region #{node['clipmx']['rds']['region']} \
+			--region=#{node['clipmx']['rds']['region']} \
 			--db-snapshot-identifier=#{node['clipmx']['rds']['instancename']}-$(date +%Y-%m-%d-%H-%M-%S) \
-			--db-instance-identifier #{node['clipmx']['rds']['instancename']}
+			--db-instance-identifier=#{node['clipmx']['rds']['instancename']}
 	EOH
 end
