f95e7f9ff9d5b27a6bd00924bfe07a34fff15da8
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-04 19:26:23 -0500

    Test default attributes

--- rdssnapshot/recipes/snapshot.rb
+++ rdssnapshot/recipes/snapshot.rb
@@ -1,4 +1,4 @@
 # AWS OpsWorks Recipe for RDS that creates snapshots.
 # - Creates a snapshot of associated RDS instances
 
-Chef::Log.info(node[:opsworks][:layers]['dev-test02'][:id])
+Chef::Log.info("#{node[:opsworks][:layers]['dev-test02'][:id]}")
