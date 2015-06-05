3b74d4a4d48d39c0170814ee2905556dc14ea749
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-04 19:44:12 -0500

    Test command for automated rds snapshot

--- rdssnapshot/recipes/snapshot.rb
+++ rdssnapshot/recipes/snapshot.rb
@@ -4,6 +4,11 @@
 Chef::Log.info("Creating snapshot for: #{node['clipmx']['rds']['instancename']}")
 
 Chef::Log.info("Importing Wordpress database backup...")
+Chef::Log.info("aws rds aws rds create-db-snapshot \
+			--region #{node['clipmx']['rds']['region']} \
+			--db-snapshot-identifier=#{node['clipmx']['rds']['instancename']}-$(date +%Y-%m-%d-%H-%M-%S) \
+			--db-instance-identifier #{node['clipmx']['rds']['instancename']}")
+			
 script "create_snapshot" do
 	interpreter "bash"
 	
