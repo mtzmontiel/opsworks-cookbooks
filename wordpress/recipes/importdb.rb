4bafc5634bdff91630db3f72168b77554b0f8f1c
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-09 17:22:27 -0500

    Fix resource naming

--- wordpress/recipes/importdb.rb
+++ wordpress/recipes/importdb.rb
@@ -1,12 +1,14 @@
-execute "create_db" do
-			command "/usr/bin/mysql --verbose -h #{node[:deploy][:database][:host]} -u #{node[:deploy][:database][:username]} -p#{node[:deploy][:database][:password]} #{node[:deploy][:database][:database]} < `echo create #{node[:deploy][:database][:database]}` > /tmp/out.tmp"
-			not_if "echo \"/* ping */ SELECT 1\" | /usr/bin/mysql --verbose -h #{node[:deploy][:database][:host]} -u #{node[:deploy][:database][:username]} -p#{node[:deploy][:database][:password]} #{node[:deploy][:database][:database]}" 
-		end
-
 node[:deploy].each do |app_name, deploy|
-	Dir["#{node[:deploy][:deploy_to]}/current/*.sql"].each do |path|
-		execute "run_sql_#{path}" do
-			command "echo \"/* ping */ SELECT 1\" | /usr/bin/mysql --verbose -h #{node[:deploy][:database][:host]} -u #{node[:deploy][:database][:username]} -p#{node[:deploy][:database][:password]} #{node[:deploy][:database][:database]} < #{path} | tee /tmp/out.tmp"
+	execute "create_db" do
+				command "/usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} < `echo create #{deploy[:database][:database]}` > /tmp/out.tmp"
+				not_if "echo \"/* ping */ SELECT 1\" | /usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]}" 
+			end
+
+	node[:deploy].each do |app_name, deploy|
+		Dir["#{deploy[:deploy_to]}/current/*.sql"].each do |path|
+			execute "run_sql_#{path}" do
+				command "echo \"/* ping */ SELECT 1\" | /usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} < #{path} | tee /tmp/out.tmp"
+			end
 		end
 	end
 end
