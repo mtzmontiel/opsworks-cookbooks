af7018a77935574a02126ba3a93fbd4b413240a7
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-09 16:19:38 -0500

    Verbose output

--- wordpress/recipes/importdb.rb
+++ wordpress/recipes/importdb.rb
@@ -1,7 +1,7 @@
 node[:deploy].each do |app_name, deploy|
 	Dir["#{deploy[:deploy_to]}/current/*.sql"].each do |path|
 		execute "run_sql_#{path}" do
-			command "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} < #{path}"
+			command "/usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} < #{path} > /tmp/out.tmp"
 		end
 	end
 end
