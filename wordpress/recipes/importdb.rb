04635bff93cd456dacec276d9e26336ac15dfdae
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-09 16:46:27 -0500

    Test connection first and create database if not exists

--- wordpress/recipes/importdb.rb
+++ wordpress/recipes/importdb.rb
@@ -1,7 +1,12 @@
+execute "run_sql_#{path}" do
+			command "/usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} < `echo create #{deploy[:database][:database]}` > /tmp/out.tmp"
+			not_if "echo \"/* ping */ SELECT 1\" | /usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]}" 
+		end
+
 node[:deploy].each do |app_name, deploy|
 	Dir["#{deploy[:deploy_to]}/current/*.sql"].each do |path|
 		execute "run_sql_#{path}" do
-			command "/usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} < #{path} > /tmp/out.tmp"
+			command "echo \"/* ping */ SELECT 1\" | /usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} < #{path} | tee /tmp/out.tmp"
 		end
 	end
 end
