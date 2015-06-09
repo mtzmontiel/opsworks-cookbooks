d74278e1f36cf41195553471b6468e69fa8afb6a
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-09 15:40:52 -0500

    Simplyfy test

--- wordpress/recipes/importdb.rb
+++ wordpress/recipes/importdb.rb
@@ -1,21 +1,21 @@
-require 'database'
-
-
-mysql_connection_info = {
-  :host     => deploy[:database][:host],
-  :username => deploy[:database][:username],
-  :password => deploy[:database][:password]
-}
+#require 'database'
+#
+#
+#mysql_connection_info = {
+#  :host     => deploy[:database][:host],
+#  :username => deploy[:database][:username],
+#  :password => deploy[:database][:password]
+#}
 
 # Import Wordpress database backup from file if it exists
 mysql_command = "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]}"
 
-Chef::Log.info("Importing Wordpress database backup...")
+Chef::Log.info("Importing Wordpress database backup: #{deploy[:deploy_to]}/current/*.sql")
 Dir["#{deploy[:deploy_to]}/current/*.sql"].each do |path|
-	mysql_database "#{deploy[:database][:database]}" do
-		connection mysql_connection_info
-		sql "source #{path};"
-	end
+#	mysql_database "#{deploy[:database][:database]}" do
+#		connection mysql_connection_info
+#		sql "source #{path};"
+#	end
 	execute "run_sql_#{path}" do
 		command "#{mysql_command} < #{path}"
 	end
