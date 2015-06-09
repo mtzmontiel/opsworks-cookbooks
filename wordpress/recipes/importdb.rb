ae751caed40eb55a58ce80d4f4dd1737ed0344f0
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-09 17:51:13 -0500

    Fixed db creation

--- wordpress/recipes/importdb.rb
+++ wordpress/recipes/importdb.rb
@@ -1,13 +1,13 @@
 node[:deploy].each do |app_name, deploy|
 	execute "create_db" do
-				command "/usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} < `echo create #{deploy[:database][:database]}` |tee  /tmp/create.db.log"
+				command "echo \"create #{deploy[:database][:database]};\" | /usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]}  | tee  /tmp/create.db.log"
 				not_if "echo \"/* ping */ SELECT 1\" | /usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]}" 
 			end
 
 	node[:deploy].each do |app_name, deploy|
 		Dir["#{deploy[:deploy_to]}/current/*.sql"].each do |path|
 			execute "run_sql_#{path}" do
-				command "echo \"/* ping */ SELECT 1\" | /usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} < #{path} | tee /tmp/import.db.log"
+				command "/usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} < #{path} | tee /tmp/import.db.log"
 			end
 		end
 	end
