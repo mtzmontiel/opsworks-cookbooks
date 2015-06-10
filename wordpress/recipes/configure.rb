51a9e447a4a449cbe1d87b19dcd7958f0d12d6fe
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-10 18:10:38 -0500

    Force https

--- wordpress/recipes/configure.rb
+++ wordpress/recipes/configure.rb
@@ -1,7 +1,6 @@
 # AWS OpsWorks Recipe for Wordpress to be executed during the Configure lifecycle phase
 # - Creates the config file wp-config.php with MySQL data.
 # - Creates a Cronjob.
-# - Imports a database backup if it exists.
 
 require 'uri'
 require 'net/http'
