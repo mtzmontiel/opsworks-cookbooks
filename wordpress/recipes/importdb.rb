bd1fcddf668972586d00fc9db95cfa21afa16ad0
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-17 16:33:51 -0500

    Force update on db for site home and site url

--- wordpress/recipes/importdb.rb
+++ wordpress/recipes/importdb.rb
@@ -11,4 +11,13 @@ node[:deploy].each do |app_name, deploy|
 			end
 		end
 	end
+	# Update siteurl and home
+	node[:deploy].each do |app_name, deploy|
+		execute "set_siteurl" do
+			command "echo \"update wp_options set option_value='#{node['wordpress']['wp_config']['siteurl']}' where option_id= 1 and option_name='siteurl';\" | /usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} | tee /tmp/import.db.log"
+		end
+		execute "set_sitehome" do
+			command "echo \"update wp_options set option_value='#{node['wordpress']['wp_config']['sitehome']}' where option_id= 2 and option_name='home';\" | /usr/bin/mysql --verbose -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} | tee /tmp/import.db.log"
+		end
+	end
 end
