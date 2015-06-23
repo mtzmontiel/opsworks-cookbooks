41f8502c118e84177a8191eca975b8fd8d0c3a5e
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-23 13:33:23 -0500

    Remove unused code

--- rails-configdb/recipes/configure.rb
+++ rails-configdb/recipes/configure.rb
@@ -1,8 +1,6 @@
 # Create the Wordpress config file database.yml with corresponding values
 node[:deploy].each do |app_name, deploy|
-	Chef::Log.info("WP force secure logins: #{node['wordpress']['wp_config']['force_secure_logins']}")
-	Chef::Log.info("WP site url: #{node['wordpress']['wp_config']['siteurl']}")
-	Chef::Log.info("WP site home: #{node['wordpress']['wp_config']['sitehome']}")
+	Chef::Log.info("Create database config file")
     template "#{deploy[:deploy_to]}/current/config/database.yml" do
         source "database.erb"
         mode 0660
@@ -13,15 +11,5 @@ node[:deploy].each do |app_name, deploy|
         elsif platform?("amazon")
           owner "apache"
         end
-
-        variables(
-            :database   => (deploy[:database][:database] rescue nil),
-            :user       => (deploy[:database][:username] rescue nil),
-            :password   => (deploy[:database][:password] rescue nil),
-            :host       => (deploy[:database][:host] rescue nil),
-            :keys       => (keys rescue nil),
-            :siteurl    => (node['wordpress']['wp_config']['siteurl'] rescue nil),
-            :sitehome   => (node['wordpress']['wp_config']['sitehome'] rescue nil),
-        )
     end
 end
