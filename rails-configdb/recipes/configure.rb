05802806615c03f0af2a4f7d02ef7433d8a71989
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-23 13:44:39 -0500

    Changed path and verifications for rails database config

--- rails-configdb/recipes/configure.rb
+++ rails-configdb/recipes/configure.rb
@@ -1,15 +1,15 @@
 # Create the Wordpress config file database.yml with corresponding values
 node[:deploy].each do |app_name, deploy|
 	Chef::Log.info("Create database config file")
-    template "#{deploy[:deploy_to]}/current/config/database.yml" do
-        source "database.erb"
-        mode 0660
-        group deploy[:group]
-
-        if platform?("ubuntu")
-          owner "www-data"
-        elsif platform?("amazon")
-          owner "apache"
-        end
-    end
+    template "#{deploy[:deploy_to]}/shared/config/database.yml" do
+		source "database.yml.erb"
+		cookbook 'rails'
+		mode "0660"
+		group deploy[:group]
+		owner deploy[:user]
+		notifies :run, "execute[restart Rails app #{application}]"
+		only_if do
+		  File.directory?("#{deploy[:deploy_to]}/shared/config/")
+		end
+	end
 end
