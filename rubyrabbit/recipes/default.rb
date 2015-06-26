f5fb7b0c4b32292b0459353ef0fcb7e410e16170
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-26 17:34:59 -0500

    Remove $(date)

--- rubyrabbit/recipes/default.rb
+++ rubyrabbit/recipes/default.rb
@@ -13,13 +13,14 @@ node[:deploy].each do |application, deploy|
         # Retrieve process id for bunny_worker
         RABBIT_PROC=` ps aux | grep bunny_worker | awk '{print $2}' `
         if [ $? -eq 0 ]
-           then echo $(date ) " Removing process: " $RABBIT_PROC | tee -a ./log/bunny_worker.stop
+           then 
+              echo " Removing process: " $RABBIT_PROC | tee -a ./log/bunny_worker.stop
               kill $RABBIT_PROC
         else 
-            echo $(date ) " No bunny_worker process. nothing to kill." $RABBIT_PROC " ." | tee -a ./log/bunny_worker.stop
+            echo  " No bunny_worker process. nothing to kill." $RABBIT_PROC " ." | tee -a ./log/bunny_worker.stop
         fi
         nohup bundle exec rake bunny_worker > ./log/bunny_worker.log &
-        echo $(date) " Done starting up bunny_worker" | tee -a ./log/bunny_worker.start
+        echo  " Done starting up bunny_worker" | tee -a ./log/bunny_worker.start
       EOH
   end
 end
