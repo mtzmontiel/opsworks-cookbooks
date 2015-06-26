0293873fbbfe6ce0bc3a535167d9a2f1c01ef803
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-26 17:35:49 -0500

    Remove tee

--- rubyrabbit/recipes/default.rb
+++ rubyrabbit/recipes/default.rb
@@ -14,13 +14,13 @@ node[:deploy].each do |application, deploy|
         RABBIT_PROC=` ps aux | grep bunny_worker | awk '{print $2}' `
         if [ $? -eq 0 ]
            then 
-              echo " Removing process: " $RABBIT_PROC | tee -a ./log/bunny_worker.stop
+              echo " Removing process: " $RABBIT_PROC >> ./log/bunny_worker.stop
               kill $RABBIT_PROC
         else 
-            echo  " No bunny_worker process. nothing to kill." $RABBIT_PROC " ." | tee -a ./log/bunny_worker.stop
+            echo  " No bunny_worker process. nothing to kill." $RABBIT_PROC " ." >>  ./log/bunny_worker.stop
         fi
         nohup bundle exec rake bunny_worker > ./log/bunny_worker.log &
-        echo  " Done starting up bunny_worker" | tee -a ./log/bunny_worker.start
+        echo  " Done starting up bunny_worker" >> ./log/bunny_worker.start
       EOH
   end
 end
