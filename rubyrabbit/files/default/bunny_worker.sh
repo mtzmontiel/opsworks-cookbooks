633303beac7a955699befb9430cf708903640bf8
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-26 18:04:28 -0500

    use tee and grep -v grep

--- rubyrabbit/files/default/bunny_worker.sh
+++ rubyrabbit/files/default/bunny_worker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
-RABBIT_PROC=` ps aux | grep bunny_worker | awk '{print $2}' `
+RABBIT_PROC=` ps aux | grep " bundle exec bunny_worker" | grep -v grep | awk '{print $2}' `
 if [ $? -eq 0 ]
    then 
-      echo " Removing process: " $RABBIT_PROC >> ./log/bunny_worker.stop
+      echo " Removing process: " $RABBIT_PROC | tee -a ./log/bunny_worker.stop
       kill $RABBIT_PROC
 else 
-    echo  " No bunny_worker process. nothing to kill." $RABBIT_PROC " ." >>  ./log/bunny_worker.stop
+    echo  " No bunny_worker process. nothing to kill." $RABBIT_PROC " ." | tee -a  ./log/bunny_worker.stop
 fi
 nohup bundle exec rake bunny_worker > ./log/bunny_worker.log &
-echo  " Done starting up bunny_worker" >> ./log/bunny_worker.start
+echo  " Done starting up bunny_worker" | tee -a ./log/bunny_worker.start
