aa99ad082c9cf219b657f3f70925fbf985584ee5
Rafael Martínez Montiel <rafael.martinez.montiel@gmail.com>
2015-06-26 16:43:53 -0500

    Force stop bunny_worker process

--- rubyrabbit/recipes/default.rb
+++ rubyrabbit/recipes/default.rb
@@ -12,8 +12,10 @@ node[:deploy].each do |application, deploy|
       # Retrieve process id for bunny_worker
       RABBIT_PROC=` ps aux | grep bunny_worker | awk '{print $2}' `
       if [ $? -eq 0 ]
-         then echo "Removing process: " $RABBIT_PROC 
-      else echo "No bunny_worker process. nothing to kill." $RABBIT_PROC " ."
+         then echo "Removing process: " $RABBIT_PROC | tee -a ./log/bunny_worker.stop
+            kill $RABBIT_PROC
+          done
+      else echo "No bunny_worker process. nothing to kill." $RABBIT_PROC " ." | tee -a ./log/bunny_worker.stop
       fi
       nohup bundle exec rake bunny_worker > ./log/bunny_worker.log &
       echo "Done starting up bunny_worker" $(date) >> ./log/bunny_worker.start
